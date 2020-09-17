e6946bf19f01185a70240bfc804257deccb480ee
wushangkun <wushangkun@chelun.com>
2020-09-17 18:59:12 +0800

    Update  _SplashWidgetState

---
+++ flutter_app1/lib/container_page.dart
@@ -0,0 +1,110 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app1/flexbox_layout.dart';
+
+class ContainerPage extends StatefulWidget {
+  ContainerPage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    print('createState _ContainerPageState');
+    return _ContainerPageState();
+  }
+}
+
+class TabItem {
+  String name, normalIcon, selectedIcon;
+  TabItem(this.name, this.normalIcon, this.selectedIcon);
+}
+
+class _ContainerPageState extends State<ContainerPage> {
+
+  int selectedIndex;
+  List<Widget> pages;
+
+  List<BottomNavigationBarItem> bottomBarItems;
+
+  final defaultItemColor = Color.fromARGB(255, 125, 125, 125);
+  final items = [
+    TabItem('首页', 'assets/images/tab_home_normal@2x.png' ,'assets/images/tab_home_selected@2x.png'),
+    TabItem('我的', 'assets/images/tab_mine_normal@2x.png' ,'assets/images/tab_mine_selected@2x.png'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState _ContainerPageState');
+
+    if(pages == null){
+      pages = [
+        MyHomePage(),
+        MyHomePage()
+      ];
+    }
+
+    print('pages >>>>>>> _ContainerPageState $pages');
+
+    if(bottomBarItems == null) {
+      bottomBarItems = items.map((item) => BottomNavigationBarItem(
+        icon: Image.asset(
+          item.normalIcon,
+          width: 30.0,
+          height: 30.0,
+        ),
+        title: Text(
+          item.name,
+          style: TextStyle(fontSize: 10.0),
+        ),
+        activeIcon: Image.asset(
+          item.selectedIcon, width: 30.0, height: 30.0
+        ),
+      )).toList();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ContainerPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('didUpdateWidget _ContainerPageState');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    print('build _ContainerPageState');
+    return Scaffold(
+      body: Stack(
+        children: [
+          _getPagesWidget(0),
+          _getPagesWidget(1),
+        ],
+      ),
+      backgroundColor: Color.fromARGB(255, 248, 248, 248),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomBarItems,
+        onTap: (int index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        iconSize: 24,
+        currentIndex: selectedIndex,
+        //选中后
+        fixedColor: Color.fromARGB(255, 0, 188, 96),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  // Stack（层叠布局）+Offstage组合,解决状态被重置的问题
+  Widget _getPagesWidget(int index) {
+    return Offstage(
+      offstage: selectedIndex != index,
+      child: TickerMode(
+        enabled: selectedIndex == index,
+        child: pages[index],
+      ),
+    );
+  }
+}
--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -1,194 +1,66 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:flutter_app1/splash/splash_widget.dart';
 
-// 导入english_words库
-import 'package:english_words/english_words.dart';
+void main() {
+  runApp(MyApp());
+  if (Platform.isAndroid) {
+    //设置Android头部的导航栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle =
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
-// void main() {
-//   runApp(MyApp());
-// }
-
-// 主函数（main）使用了 (=>) 符号，这是 Dart 中单行函数或方法的简写
-void main() => runApp(MyApp());
-
-//应用程序继承了 StatelessWidget，这将会使应用本身也成为一个 widget。在 Flutter 中，几乎所有都是 widget，
-//包括对齐 (alignment)、填充 (padding) 和布局 (layout)。
 class MyApp extends StatelessWidget {
   @override
-  // 一个 widget 的主要工作是提供一个 build() 方法来描述如何根据其他较低级别的 widgets 来显示自己。
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-         // 主题外观
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+
+    return RestartWidget(
+      child: MaterialApp(
+        theme: ThemeData(backgroundColor: Colors.white),
+        home: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: SplashWidget(),
+        ),
       ),
-      home: MyHomePage(title: 'wushangkun'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+///这个组件用来重新加载整个child Widget的。当我们需要重启APP的时候，可以使用这个方案
+class RestartWidget extends StatefulWidget {
+  final Widget child;
 
-  final String title;
+  RestartWidget({Key key, @required this.child})
+      : assert(child != null),
+        super(key: key);
+
+  static restartApp(BuildContext context) {
+    final _RestartWidgetState state =
+    context.ancestorStateOfType(const TypeMatcher<_RestartWidgetState>());
+    state.restartApp();
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RestartWidgetState createState() => _RestartWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
 
-  int _counter = 0;
-
-  final wordPair = WordPair.random();
-
-  // 添加一个 _suggestions 列表保存单词对
-  final List<WordPair> _suggestions = <WordPair>[];
-
-  // 这个集合存储用户喜欢（收藏）的单词对
-  final Set<WordPair> _saved =  Set<WordPair>();
-
-  // 添加一个 _biggerFont 变量来增大字体大小。
-  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    // 工厂匿名回调函数
-    // 两个参数 BuildContext 和行迭代器 i
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次 itemBuilder，然后将单词对添加到 ListTile 行中。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线 widget。
-          if (i.isOdd) return Divider();
-
-          // 在偶数行，该函数会为单词对添加一个 ListTile row，在奇数行，该函数会添加一个分割线的 widget，来分隔相邻的词对。
-          final index = i ~/ 2; // 语法 i ~/ 2 表示 i 除以 2，但返回值是整形（向下取整）
-          if (index >= _suggestions.length) {
-            // 如果是列表中最后一个单词对，接着再生成10个单词对，然后添加到列表。
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    // 检查确保单词对还没有添加到收藏夹中。
-    final bool alreadySaved = _saved.contains(pair);
-
-    return  ListTile(
-      title:  Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      // 每一行加一个心形 ❤️图标️
-      trailing:  Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        // 响应式风格的框架中，调用 setState() 会为 State 对象触发 build() 方法，从而导致对 UI 的更新
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Scaffold 是 Material 库中提供的一个 widget，它提供了默认的导航栏、标题和包含主屏幕 widget 树的 body 属性。
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          // 跳转到新页面（路由［route］）
-           IconButton(icon: const Icon(Icons.list), onPressed: _pushToSaved)
-        ],
-      ),
-
-      body: _buildSuggestions(),
-
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       Text(
-      //         'You have clicked the button this many times:',
-      //       ),
-      //       Text(
-      //         // 语言包引用， 大驼峰式命名法
-      //         wordPair.asPascalCase,
-      //         style: Theme.of(context).textTheme.headline6,
-      //       ),
-      //
-      //       // 引用 一个 stateful widget（有状态的 widget）—— RandomWords
-      //       // RandomWords(),
-      //
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headline4,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        // onPressed: () { print('FAB pressed.'); },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-
-      // floatingActionButtonLocation:
-      //     AlmostEndFloatFabLocation(offsetX: 0.0, offsetY: 0.0),
-    );
-  }
-
-  void _incrementCounter() {
+  void restartApp() {
     setState(() {
-      _counter++;
-      _suggestions.insert(0, WordPair.random());
+      key = UniqueKey();
     });
   }
 
-  void _pushToSaved() {
-    // 导航器管理应用程序的路由栈。
-    // 将路由推入（push）到导航器的栈中，将会显示更新为该路由页面。
-    // 从导航器的栈中弹出（pop）路由，将显示返回到前一个路由。
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-
-          // ListTile 的 divideTiles() 方法在每个 ListTile 之间添加 1 像素的分割线。
-          // 该 divided 变量持有最终的列表项，并通过 toList()方法非常方便的转换成列表显示。
-
-          final List<Widget> divided =
-          ListTile.divideTiles(tiles: tiles, context: context).toList();
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('我喜欢的'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: key,
+      child: widget.child,
     );
   }
 }
